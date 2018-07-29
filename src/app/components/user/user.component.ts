02a2796cad2379304e6a10f8fa244887f5a487fa
Stasia Salinas <stasiafoxx@mail.fresnostate.edu>
2018-07-29 11:16:55 -0700

    practicing methods

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -15,7 +15,31 @@ export class UserComponent {
   age: number;
   address;
 
+  foo: any;
+  hasKids: boolean;
+  numberArray: number[];
+  stringArray: string [];
+  mixedArray: any[];
+  myTuple: [string, number, boolean];
+  unusable: void;
   // Methods
   constructor () {
     this.firstName = 'Stasia';
+    this.lastName = 'Salinas';
+    this.age = 23;
+
+    this.address = {
+      street: '126 N Van Ness',
+      city: 'Fresno',
+      state: 'CA'
+    };
+
+    this.foo = true;
+    this.hasKids = false;
+    this.numberArray = [1, 2, 3];
+    this.stringArray = ['hello', 'world'];
+    this.mixedArray = [true, undefined, 'hello'];
+
   }
+
+}
